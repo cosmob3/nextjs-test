cbad1436bfad18728a2b4fc76881c49cd431c2d5
cosmob3 <3ryanvelasc0@gmail.com>
2025-04-25 00:11:05 -0600

    leatning nextJS - test

--- app/hello/page.tsx
+++ app/hello/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main>
-      <h1>Hello World</h1>
+      <h1>In the hello Page</h1>
     </main>
   );
 }
---
+++ app/users/page.tsx
@@ -0,0 +1,11 @@
+export default function UsersPage() {
+  return (
+    <div>
+      <h1 className="text-4xl text-center">Hello This is the users page</h1>
+      <p className="text-2xl text-center p-20">
+        This is just some random paragraphs to be able to manipulate and play
+        with for the time being. Testing how this works.
+      </p>
+    </div>
+  );
+}
